5e5742a3052cef3f7f2319556601b623805aa6e4
ayunav <ayunanyc@gmail.com>
2016-02-21 10:22:53 -0500

    Initial Commit

---
+++ AVHeadsUpGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVHeadsUpGame
+//
+//  Created by Ayuna Vogel on 2/21/16.
+//  Copyright © 2016 Ayuna Vogel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVHeadsUpGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVHeadsUpGame
+//
+//  Created by Ayuna Vogel on 2/21/16.
+//  Copyright © 2016 Ayuna Vogel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
